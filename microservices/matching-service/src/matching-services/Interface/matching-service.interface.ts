e9a5643aae763a8da6a9c4e3f4c52b6bb6861a80
Ziming <gzm98727@gmail.com>
2024-04-09 20:43:06 +0800

    format

--- microservices/matching-service/src/matching-services/Interface/matching-service.interface.ts
+++ microservices/matching-service/src/matching-services/Interface/matching-service.interface.ts
@@ -1,4 +1,4 @@
-import { Document } from 'mongoose';
+import { Document } from "mongoose";
 
 export interface MatchingServices extends Document {
   readonly matching_service_id: number;
--- microservices/matching-service/src/matching-services/matchingservices.controller.ts
+++ microservices/matching-service/src/matching-services/matchingservices.controller.ts
@@ -1,16 +1,25 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from "@nestjs/common";
 import { MatchingServicesService } from "./matchingservices.service";
 import { CreateMatchingServicesDto } from "./dto/create-matching-services.dto";
 import { UpdateMatchingServicesDto } from "./dto/update-matching-services.dto";
-import { MatchingService } from './schemas/matchingservices.schema';
-import { Queue } from 'bull';
-import { InjectQueue } from '@nestjs/bull';
+import { MatchingService } from "./schemas/matchingservices.schema";
+import { Queue } from "bull";
+import { InjectQueue } from "@nestjs/bull";
 
 @Controller("matching-service")
 export class MatchingServiceController {
-  constructor(private readonly matchingservice: MatchingServicesService,
-    @InjectQueue('process') private readonly msQueue: Queue,
-  ) { }
+  constructor(
+    private readonly matchingservice: MatchingServicesService,
+    @InjectQueue("process") private readonly msQueue: Queue,
+  ) {}
 
   /**
    * Create a new matching service.
@@ -19,7 +28,7 @@ export class MatchingServiceController {
    */
   @Post()
   async create(@Body() CreateMatchingServiceDto: CreateMatchingServicesDto) {
-    await this.msQueue.add('process', { data: CreateMatchingServiceDto });
+    await this.msQueue.add("process", { data: CreateMatchingServiceDto });
     return this.matchingservice.create(CreateMatchingServiceDto);
   }
 
@@ -37,8 +46,8 @@ export class MatchingServiceController {
    * @param id - The ID of the matching service.
    * @returns A promise that resolves to the matching service, or null if not found.
    */
-  @Get(':id')
-  async findOne(@Param('id') id: number): Promise<MatchingService | null> {
+  @Get(":id")
+  async findOne(@Param("id") id: number): Promise<MatchingService | null> {
     return this.matchingservice.findOne(id);
   }
 
@@ -48,8 +57,11 @@ export class MatchingServiceController {
    * @param updateMatchingServiceDto - The data for updating the matching service.
    * @returns A promise that resolves to the updated matching service, or null if not found.
    */
-  @Put(':id')
-  async update(@Param('id') id: number, @Body() updateDto: UpdateMatchingServicesDto): Promise<MatchingService | null> {
+  @Put(":id")
+  async update(
+    @Param("id") id: number,
+    @Body() updateDto: UpdateMatchingServicesDto,
+  ): Promise<MatchingService | null> {
     return this.matchingservice.update(id, updateDto);
   }
 
@@ -57,8 +69,8 @@ export class MatchingServiceController {
    * Delete a matching service by ID.
    * @param id - The ID of the matching service to delete.
    */
-  @Delete(':id')
-  async delete(@Param('id') id: number) {
+  @Delete(":id")
+  async delete(@Param("id") id: number) {
     return this.matchingservice.delete(id);
   }
 }
